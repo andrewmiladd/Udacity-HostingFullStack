6818e7a8e678bd097a26fe81bef6ab0d385d4db7
Daniel Kelly <email@danielmkelly.com>
2021-06-18 16:49:14 -0700

    feat: angular version

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,12 +1,12 @@
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: Number(process.env.PORT),
-  host: process.env.POSTGRES_HOST,
+  username: `${process.env.RDS_USERNAME}`, //`${process.env.POSTGRES_USERNAME}`,
+  password: process.env.RDS_PASSWORD, //process.env.POSTGRES_PASSWORD,
+  database: process.env.RDS_DB_NAME, //process.env.POSTGRES_DB,
+  port: Number(process.env.RDS_PORT), //Number(process.env.PORT),
+  host: process.env.RDS_HOSTNAME, //process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
