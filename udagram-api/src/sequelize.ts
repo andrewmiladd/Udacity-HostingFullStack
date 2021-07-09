faff7754860ebc1c4d61b9c4809a18ce27c2e183
Daniel Kelly <email@danielmkelly.com>
2021-07-09 14:35:16 -0700

    fix: Sequelize

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: config.username,
+  username: String(config.username),
   password: config.password,
   database: config.database,
   host: config.host,
