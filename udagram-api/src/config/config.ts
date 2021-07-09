47234b25a09e88b0427ab197477839280cbeefe6
Daniel Kelly <email@danielmkelly.com>
2021-07-09 14:04:12 -0700

    fix: db connection

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const config = {
   username: `postgres`, //`${process.env.RDS_USERNAME}`, //`${process.env.POSTGRES_USERNAME}`,
   password: `postgres123`, //process.env.RDS_PASSWORD, //process.env.POSTGRES_PASSWORD,
-  database: `udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com`,//process.env.RDS_DB_NAME, //process.env.POSTGRES_DB,
+  database: `udacityfullstack-db`,//process.env.RDS_DB_NAME, //process.env.POSTGRES_DB,
   port: Number(5432), //Number(process.env.RDS_PORT), //Number(process.env.PORT),
   host: `udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com`, //process.env.RDS_HOSTNAME, //process.env.POSTGRES_HOST,
   dialect: "postgres",
