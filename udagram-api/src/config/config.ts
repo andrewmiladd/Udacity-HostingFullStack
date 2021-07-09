589a096a91b606f87cedc1506cfc8b68d45ae3c4
Daniel Kelly <email@danielmkelly.com>
2021-07-09 16:45:00 -0700

    fix: backend database

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  username: `postgres`, //`${process.env.RDS_USERNAME}`, //`${process.env.POSTGRES_USERNAME}`,
+  username: "postgres", //`${process.env.RDS_USERNAME}`, //`${process.env.POSTGRES_USERNAME}`,
   password: "postgres123", //process.env.RDS_PASSWORD, //process.env.POSTGRES_PASSWORD,
   database: "udacityfullstack-db",//process.env.RDS_DB_NAME, //process.env.POSTGRES_DB,
-  port: Number(5432), //Number(process.env.RDS_PORT), //Number(process.env.PORT),
+  port: 5432, //Number(process.env.RDS_PORT), //Number(process.env.PORT),
   host: "udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com", //process.env.RDS_HOSTNAME, //process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,20 +2,11 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  /*
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
   port: config.port,
-  */
-
-  username: "postgres",
-  password: "postgres123",
-  database: "udacityfullstack-db",
-  host: "udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com",
-  port: 5432,
-
   dialect: "postgres",
   storage: ":memory:",
 });
