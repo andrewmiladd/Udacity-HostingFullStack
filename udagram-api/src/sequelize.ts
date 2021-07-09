6a938aef4604ab55c032a754382532ec7756d66c
Daniel Kelly <email@danielmkelly.com>
2021-07-09 14:44:51 -0700

    fix: troubleshoot seqelize

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,11 +2,19 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: String(config.username),
+  /*
+  username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
   port: config.port,
+  */
+
+  username: "postgres",
+  password: "postgres123",
+  database: "udacityfullstack-db",
+  host: "udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com",
+  port: 5432,
 
   dialect: "postgres",
   storage: ":memory:",
