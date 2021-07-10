239cb0f413cf2a42a3e396a79c0b73366d80ec9e
Daniel Kelly <email@danielmkelly.com>
2021-07-09 17:20:53 -0700

    fix: RDS variables

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  username: "postgres", //`${process.env.RDS_USERNAME}`, //`${process.env.POSTGRES_USERNAME}`,
-  password: "postgres123", //process.env.RDS_PASSWORD, //process.env.POSTGRES_PASSWORD,
-  database: "udacityfullstack-db",//process.env.RDS_DB_NAME, //process.env.POSTGRES_DB,
-  port: 5432, //Number(process.env.RDS_PORT), //Number(process.env.PORT),
-  host: "udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com", //process.env.RDS_HOSTNAME, //process.env.POSTGRES_HOST,
+  username: `${process.env.POSTGRES_USERNAME}`, //"postgres", //`${process.env.RDS_USERNAME}`, 
+  password: process.env.POSTGRES_PASSWORD,//"postgres123", //process.env.RDS_PASSWORD, 
+  database: process.env.POSTGRES_DB, // "udacityfullstack-db",//process.env.RDS_DB_NAME, //,
+  port: Number(process.env.PORT), //5432, //Number(process.env.RDS_PORT), 
+  host: process.env.POSTGRES_HOST, //"udacityfullstack-db.c944lgdehjcn.us-east-1.rds.amazonaws.com", //process.env.RDS_HOSTNAME, 
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagramapi-env.eba-qxkmez6h.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://env-udagramapi.eba-qxkmez6h.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
